cd51017d419bc6c69d11a5e277243193a5501da3
北京浣熊 <racoonbj@hotmail.com>
2024-05-19 03:09:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,7 +13,7 @@ let addresses = [
 let addressesapi = [
 	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
-	'https://https://wenben-doc.pages.dev/racoonbj/ip.txt', 
+	'https://wenben-doc.pages.dev/ip.txt?token=racoonbj', 
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
